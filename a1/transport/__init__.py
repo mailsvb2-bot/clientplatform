0e2233886d908206f1fadadacd80e60fdb2850a1
BusinesMetro <mailsvb2@gmail.com>
2026-07-28 02:33:05 +0300

    Initialize A1 transport package

---
+++ a1/transport/__init__.py
@@ -0,0 +1,14 @@
+from a1.transport.base import (
+    AdapterRegistry,
+    CredentialProvider,
+    DispatchAdapter,
+)
+from a1.transport.telegram import TelegramBotClient, TelegramDispatchAdapter
+
+__all__ = [
+    "AdapterRegistry",
+    "CredentialProvider",
+    "DispatchAdapter",
+    "TelegramBotClient",
+    "TelegramDispatchAdapter",
+]
