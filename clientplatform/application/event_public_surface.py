654acc7ccd2f36c0ccb0305ede4a480bb03f3079
BusinesMetro <mailsvb2@gmail.com>
2026-09-19 16:13:44 +0300

    ux: offer verified messenger marketing choices

--- clientplatform/application/event_public_surface.py
+++ clientplatform/application/event_public_surface.py
@@ -41,11 +41,13 @@ def render_event_landing_body(
             f"<input type=hidden name=marketing_consent_hash value='{escape(consent_hash, quote=True)}'>"
             f"<label><input style='width:auto' type=checkbox name=marketing_consent value=yes> "
             f"{escape(consent_text)}</label>"
-            "<p>Для публичной регистрации коммерческое согласие подтверждается только для E-mail. "
-            "Telegram, ВКонтакте и MAX можно безопасно привязать после регистрации для "
-            "организационных напоминаний; коммерческие сообщения в мессенджере требуют "
-            "отдельного подтверждения владения каналом.</p>"
-            "<label><input style='width:auto' type=checkbox name=marketing_channel value=email checked> E-mail</label>"
+            "<p>Выберите каналы. E-mail подтверждается этой формой. Для Telegram, "
+            "ВКонтакте и MAX после регистрации появится одноразовая кнопка: коммерческие "
+            "сообщения в мессенджере включатся только после подтверждения владения аккаунтом.</p>"
+            "<label><input style='width:auto' type=checkbox name=marketing_channel value=email checked> E-mail</label> "
+            "<label><input style='width:auto' type=checkbox name=marketing_channel value=telegram> Telegram</label> "
+            "<label><input style='width:auto' type=checkbox name=marketing_channel value=vk> ВКонтакте</label> "
+            "<label><input style='width:auto' type=checkbox name=marketing_channel value=max> MAX</label>"
             "</fieldset>"
         )
     return (
